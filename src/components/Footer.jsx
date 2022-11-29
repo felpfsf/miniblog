27d6b88cccc2a1414b21173948c43381a5b72a94
Felipe F <feferreira.sf@gmail.com>
2022-11-29 13:56:13 -0300

    style: Footer font-size corrections

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 export const Footer = () => {
   return (
-    <footer className='w-full mt-40 mx-auto p-8 bg-miniBlog-dark/70 flex flex-col items-center justify-center gap-2'>
+    <footer className='w-full mt-16 mx-auto p-8 bg-miniBlog-dark/70 flex flex-col items-center justify-center gap-2'>
       <h1 className='text-2xl'>miniBlog</h1>
-      <h3 className='text-sm font-light'>Escreva sobre o tema que te interessa</h3>
-      <p className='text-sm font-light'>MiniBlog &copy; 2022 - Desenvolvido por <a href="http://github.com/felpfsf" target="_blank" rel="noopener noreferrer" className='font-semibold underline underline-offset-4 hover:text-miniBlog-water transition-all duration-300'> Felipe F</a></p>
+      <h3 className='text-xs font-light'>Escreva sobre o tema que te interessa</h3>
+      <p className='text-xs font-light'>MiniBlog &copy; 2022 - Desenvolvido por <a href="http://github.com/felpfsf" target="_blank" rel="noopener noreferrer" className='font-semibold underline underline-offset-4 hover:text-miniBlog-water transition-all duration-300'> Felipe F</a></p>
     </footer>
   )
 }
