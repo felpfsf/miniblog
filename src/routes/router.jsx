758bf6496ac40b5fe80b7739e886203f28c53dbb
Felipe F <feferreira.sf@gmail.com>
2022-11-29 13:56:43 -0300

    feat: added Register page

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -1,9 +1,11 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
 import { Footer } from '../components/Footer'
 import { Navbar } from '../components/Navbar'
 import { About } from '../pages/About/About'
 import { Home } from '../pages/Home/Home'
 import { NotFound } from '../pages/NotFound/NotFound'
+import { Register } from '../pages/Register/Register'
 
 export const Router = () => {
   return (
@@ -12,6 +14,7 @@ export const Router = () => {
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
+        <Route path='/register' element={<Register />} />
         <Route path='/*' element={<NotFound />} />
       </Routes>
       <Footer />
