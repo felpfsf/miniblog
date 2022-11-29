e664d4c6b07e1752a9872abc4daed750b5ef1cc8
Felipe F <feferreira.sf@gmail.com>
2022-11-29 17:48:15 -0300

    feat: project description in about section page

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 export const About = () => {
   return (
     <main className='max-w-[1440px] w-full mx-auto pt-16 px-4'>
-      <h1>About</h1>
-      <p>Este é um projeto didático desenvolvido para estudar as funcionalidades do React, controles de rotas com react-router-dom, práticas em autenticação de usuário e manipulação de banco-de-dados com o Firebase.</p>
+      <h1 className='text-lg font-medium'>O que é o miniBlog?</h1>
+      <p className='font-light text-white/80 leading-relaxed mt-4'>Este é um projeto didático desenvolvido para estudar as funcionalidades do <strong>React</strong>, controles de rotas com <strong>React-Router-Dom</strong>, práticas em autenticação de usuário e manipulação de banco-de-dados com o <strong>Firebase</strong>.</p>
     </main>
   )
 }
