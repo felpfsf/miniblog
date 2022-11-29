9db23f6749e682307433de3fa42ed68f42078fcb
Felipe F <feferreira.sf@gmail.com>
2022-11-29 13:55:31 -0300

    feat: added project description

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -4,6 +4,7 @@ export const About = () => {
   return (
     <main className='max-w-[1440px] w-full mx-auto pt-16 px-4'>
       <h1>About</h1>
+      <p>Este é um projeto didático desenvolvido para estudar as funcionalidades do React, controles de rotas com react-router-dom, práticas em autenticação de usuário e manipulação de banco-de-dados com o Firebase.</p>
     </main>
   )
 }
