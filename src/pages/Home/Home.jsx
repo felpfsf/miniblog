c167a0324cec1e3a85113d6e6cba08956e91757a
Felipe F <feferreira.sf@gmail.com>
2022-11-29 22:27:28 -0300

    style: change font style

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export const Home = () => {
   return (
     <main className='max-w-[1440px] w-full mx-auto pt-16 px-4'>
-      <h1>Home</h1>
+      <h1 className='text-2xl font-black'>Home</h1>
     </main>
   )
 }
