9f5e16ce1d14255a9f7dfa7e60f9fffec76038af
Felipe F <feferreira.sf@gmail.com>
2022-11-30 14:08:42 -0300

    feat: prettier format

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,9 +1,9 @@
-import { LoginForm } from "../../components/LoginForm"
+import { LoginForm } from '../../components/LoginForm'
 
 export const Login = () => {
   return (
-    <main className="max-w-[1440px] w-full mx-auto pt-16 px-4 flex flex-col items-center">
-      <h1 className="text-xl font-medium">Efetuar login</h1>
+    <main className='max-w-[1440px] w-full mx-auto pt-16 px-4 flex flex-col items-center'>
+      <h1 className='text-xl font-medium'>Efetuar login</h1>
       <LoginForm />
     </main>
   )
