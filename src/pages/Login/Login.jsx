c5dcaa4f9c20e04ff5266aaa7330a2ca42b1ce83
Felipe F <feferreira.sf@gmail.com>
2022-11-29 22:56:56 -0300

    feat: added Login page

---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,8 @@
+
+export const Login = () => {
+  return (
+    <main className="max-w-[1440px] w-full mx-auto pt-16 px-4">
+      <h1 className="text-2xl font-black">Login</h1>
+    </main>
+  )
+}
