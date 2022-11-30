1cf2e9878d2cfb3669a4331448e7cb797a8d4f2f
Felipe F <feferreira.sf@gmail.com>
2022-11-30 18:04:47 -0300

    feat: set min-height value and added post form

--- src/pages/CreatePost/CreatePost.jsx
+++ src/pages/CreatePost/CreatePost.jsx
@@ -1,8 +1,11 @@
+import { PostForm } from '../../components/PostForm'
 
 export const CreatePost = () => {
   return (
-    <main className="max-w-[1440px] w-full mx-auto pt-16 px-4">
-      <h1 className="text-2xl font-black">CreatePost</h1>
+    <main className='max-w-[1440px] min-h-[calc(100vh_-_208px)] w-full mx-auto pt-16 px-4 flex flex-col items-center'>
+      <h1 className='text-2xl font-black'>Criar um novo post</h1>
+      <h2>Escreva sobre o que quiser</h2>
+      <PostForm />
     </main>
   )
 }
