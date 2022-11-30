9a31508054f3969e4d84dea5b521292e05ccc248
Felipe F <feferreira.sf@gmail.com>
2022-11-29 23:45:41 -0300

    refactor: change Form component name to RegisterForm

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,10 +1,10 @@
-import { Form } from "../../components/Form"
+import { RegisterForm } from "../../components/RegisterForm"
 
 export const Register = () => {
   return (
     <main className='max-w-[1440px] w-full mx-auto pt-16 px-4 flex flex-col items-center'>
       <h1 className="text-xl font-medium">Registrar</h1>
-      <Form />
+      <RegisterForm />
     </main>
   )
 }
