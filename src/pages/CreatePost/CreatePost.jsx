9199ce5175aecedde2a1e7680700acf8b3894303
Felipe F <feferreira.sf@gmail.com>
2022-11-29 22:56:10 -0300

    feat: added create post page to test protected route

---
+++ src/pages/CreatePost/CreatePost.jsx
@@ -0,0 +1,8 @@
+
+export const CreatePost = () => {
+  return (
+    <main className="max-w-[1440px] w-full mx-auto pt-16 px-4">
+      <h1 className="text-2xl font-black">CreatePost</h1>
+    </main>
+  )
+}
