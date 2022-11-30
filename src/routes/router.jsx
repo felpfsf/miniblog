028154ee741c4f37bb8dc4007563f77703761c45
Felipe F <feferreira.sf@gmail.com>
2022-11-29 22:29:02 -0300

    feat: added protected routes

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -1,4 +1,9 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { useEffect, useState } from 'react'
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
+
+import { AuthProvider } from '../context/AuthContext'
+import { useAuthentication } from '../hooks/useAuthentication'
+
 
 import { Footer } from '../components/Footer'
 import { Navbar } from '../components/Navbar'
@@ -6,18 +11,66 @@ import { About } from '../pages/About/About'
 import { Home } from '../pages/Home/Home'
 import { NotFound } from '../pages/NotFound/NotFound'
 import { Register } from '../pages/Register/Register'
+import { onAuthStateChanged } from 'firebase/auth'
+import { CreatePost } from '../pages/CreatePost/CreatePost'
+import { Dashboard } from '../pages/Dashboard/Dashboard'
+import { Login } from '../pages/Login/Login'
 
 export const Router = () => {
+  const [user, setUser] = useState(undefined)
+  const { auth } = useAuthentication()
+
+  const loadingUser = user === undefined
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      setUser(user)
+    })
+  }, [auth])
+
+  if (loadingUser) {
+    return <p>Carregando...</p>
+  }
+
+
   return (
-    <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/about' element={<About />} />
-        <Route path='/register' element={<Register />} />
-        <Route path='/*' element={<NotFound />} />
-      </Routes>
-      <Footer />
-    </BrowserRouter>
+    <AuthProvider value={{ user }}>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/dashboard'
+            element={user
+              ? <Dashboard />
+              : <Navigate to={'/login'} />
+            }
+          />
+          <Route path='/login'
+            element={
+              !user
+                ? <Login />
+                : <Navigate to={'/'} />
+            }
+          />
+          <Route path='/register'
+            element={
+              !user
+                ? <Register />
+                : <Navigate to={'/'} />
+            }
+          />
+          <Route path='/posts/create'
+            element={
+              user
+                ? <CreatePost />
+                : <Navigate to={'/login'} />
+            }
+          />
+          <Route path='/*' element={<NotFound />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </AuthProvider>
   )
 }
