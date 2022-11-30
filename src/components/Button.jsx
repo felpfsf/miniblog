7f71311e2f5c1d6c465bdcf6ac0f8a1883a4b89c
Felipe F <feferreira.sf@gmail.com>
2022-11-30 13:54:18 -0300

    style: margin-top correction

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,7 @@
 export const Button = ({buttonText, disabled}) => {
   return (
     <button 
-    className='mt-4 py-2 px-8 bg-miniBlog-primary rounded self-center hover:contrast-200 transition-color ease-in-out duration-300'
+    className='mt-2 py-2 px-8 bg-miniBlog-primary rounded self-center hover:contrast-200 transition-color ease-in-out duration-300'
     disabled={disabled}
     >
       {buttonText}
