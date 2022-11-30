15c495727c759c00473293713f2cea3acd476290
Felipe F <feferreira.sf@gmail.com>
2022-11-29 23:44:18 -0300

    feat: added login form

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,8 +1,10 @@
+import { LoginForm } from "../../components/LoginForm"
 
 export const Login = () => {
   return (
-    <main className="max-w-[1440px] w-full mx-auto pt-16 px-4">
-      <h1 className="text-2xl font-black">Login</h1>
+    <main className="max-w-[1440px] w-full mx-auto pt-16 px-4 flex flex-col items-center">
+      <h1 className="text-xl font-medium">Efetuar login</h1>
+      <LoginForm />
     </main>
   )
 }
