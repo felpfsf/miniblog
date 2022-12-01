1ff2d06634f2c4a3ad91832493cce862478e2ea2
Felipe F <feferreira.sf@gmail.com>
2022-12-01 20:26:31 -0300

    feat: Component for no posts

---
+++ src/components/NoPostFound.jsx
@@ -0,0 +1,14 @@
+import { Link } from "react-router-dom"
+
+export const NoPostFound = () => {
+  return (
+    <div className='w-full mt-4 mx-auto flex flex-col items-center'>
+      <h2 className='font-light'>Nenhum post encontrado</h2>
+      <Link
+        to={'/posts/create'}
+        className='mt-2 py-2 px-8 bg-miniBlog-primary rounded self-center hover:contrast-200 transition-color ease-in-out duration-300'>
+        Seja o primeiro a postar
+      </Link>
+    </div>
+  )
+}
