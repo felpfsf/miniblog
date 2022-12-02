6e34901f6b27a2f7f7764083765f83e39b11b976
Felipe F <feferreira.sf@gmail.com>
2022-12-02 11:45:12 -0300

    refactor: post card style refactor

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -1,18 +1,48 @@
-import { Link } from "react-router-dom"
+import { Link } from 'react-router-dom'
+
+import { HiOutlineArrowSmRight } from 'react-icons/hi'
+import { BsArrowRightShort } from 'react-icons/bs'
 
 export const PostCard = ({ postImgUrl, id, postTitle, tags }) => {
   return (
-    <div className='w-[336px] mt-4 mx-auto p-2 flex flex-col gap-4'>
-      <div className='w-full h-[336px] rounded-lg overflow-hidden'>
-        <img src={postImgUrl} alt="" className='w-full h-full' />
+    <div class='p-4 md:w-1/3'>
+      <div class='h-full border-2 border-miniBlog-bg3 rounded-lg overflow-hidden'>
+        <img
+          class='lg:h-48 md:h-36 w-full object-cover object-center'
+          src={postImgUrl}
+          alt={postTitle}
+        />
+        <div class='p-6'>
+          <h2 class='tracking-widest text-xs title-font font-medium text-gray-500 uppercase mb-1'>
+            Categoria
+          </h2>
+          <div className='flex gap-3 self-center'>
+            {tags.map((tag, index) => (
+              <p
+                key={index}
+                className='py-1 px-2 text-xs font-light leading-4 rounded-2xl border transition-all duration-300 hover:bg-white hover:text-miniBlog-text cursor-pointer'>
+                <span className='font-medium'>#</span>
+                {tag}
+              </p>
+            ))}
+          </div>
+          <h1 class='title-font text-lg font-medium text-white my-3'>
+            {postTitle}
+          </h1>
+          <p class='text-white/60 leading-relaxed mb-3'>
+            Photo booth fam kinfolk cold-pressed sriracha leggings jianbing
+            microdosing tousled waistcoat.
+          </p>
+          <div class='flex items-center flex-wrap '>
+            <Link
+              to={`/posts/${id}`}
+              class='text-white p-2 border inline-flex items-center md:mb-2 lg:mb-0 duration-300 hover:bg-white hover:text-miniBlog-bg2'>
+              Leia mais
+              <BsArrowRightShort size={24} className=' ml-2' />
+            </Link>
+          </div>
+        </div>
       </div>
-      <h1 className="text-lg font-medium">{postTitle}</h1>
-      <div className='flex gap-3 self-center'>
-        {tags.map((tag, index) => (
-          <p key={index} className='py-1 px-2 text-sm font-light leading-4 rounded-2xl border transition-all duration-300 hover:bg-white hover:text-miniBlog-text cursor-pointer'><span className="font-medium">#</span>{tag}</p>
-        ))}
-      </div>
-      <Link to={`/posts/${id}`} className='w-24 text-lg leading-[0.8] text-white p-2 border transition-all duration-300 hover:bg-white hover:text-miniBlog-bg2'>Ler</Link>
     </div>
   )
 }
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -15,14 +15,14 @@ export const Home = () => {
     <main className='max-w-[1440px] w-full min-h-[calc(100vh_-_208px)] mx-auto pt-16 px-4'>
       <h1 className='text-xl font-black'>Veja os posts mais recentes</h1>
       <SearchForm />
-      <div className='h-[1px] w-full mt-4 bg-white/60'></div>
+      <div className='h-[1px] w-full my-4 bg-white/60'></div>
 
       {!loading ? <h2>Carregando...</h2> : null}
-
-      {posts ? posts.map(post =>
-        <PostCard key={post.id} {...post} />
-      ) : null}
-
+      <div className="flex flex-wrap -m-4">
+        {posts ? posts.map(post =>
+          <PostCard key={post.id} {...post} />
+        ) : null}
+      </div>
       {posts && posts.length === 0 ? (
         <NoPostFound />
       ) : null}
