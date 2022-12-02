9a515547a83b2bbd5c92dca606d4c5c460727177
Felipe F <feferreira.sf@gmail.com>
2022-12-02 12:48:13 -0300

    refactor: detailed post page revampe mobile and desktop

--- src/components/PostDetailed.jsx
+++ src/components/PostDetailed.jsx
@@ -1,21 +1,33 @@
-export const PostDetailed = ({ createdBy, postImgUrl, postMsg, postTitle, tags }) => {
+export const PostDetailed = ({ createdBy, imgUrl, message, title, tags }) => {
   return (
-    <div className="max-w-sm w-full mx-auto mb-4 flex flex-col gap-4">
-      <h1 className="text-2xl font-black">{postTitle}</h1>
-      <div className='w-full h-[336px] rounded-lg overflow-hidden'>
-        <img src={postImgUrl} alt={postTitle} className='w-full h-full' />
-      </div>
-      <div className="">
-        <p className="text-sm text-white/80 text-justify">{postMsg}</p>
-      </div>
-      <div className="flex flex-wrap gap-3">
-        {tags.map((tag, index) => (
-          <p key={index} className='w-auto py-1 px-2 text-sm font-light leading-4 rounded-2xl border transition-all duration-300 hover:bg-white hover:text-miniBlog-text cursor-pointer'><span className="font-medium">#</span>{tag}</p>
-        ))}
-      </div>
-      <div className="h-[1px] w-full mt-2 bg-white/60"></div>
-      <div>
-        <p className="leading-[2px] text-white/60">Postado por: <span className="ml-2">{createdBy}</span></p>
+    <div className='md:w-2/3 mb-4 mx-auto'>
+      <div className="w-full">
+        <img
+          className='w-full h-48 sm:h-96 object-cover'
+          src={imgUrl}
+          alt={title}
+        />
+        <div className="p-4">
+          <p className=" text-sm text-white/60 leading-[2px]">{createdBy}</p>
+          <h1 className="text-lg font-medium my-3">{title}</h1>
+          <p className="text-sm text-white/80 text-justify">
+            {message}
+          </p>
+          <div className="h-[1px] w-full bg-white/60 my-3"></div>
+          <h2 className='tracking-widest text-xs font-medium text-miniBlog-greySoft/70 uppercase mb-1'>
+            Categoria
+          </h2>
+          <div className='flex gap-3 self-center'>
+            {tags.map((tag, index) => (
+              <p
+                key={index}
+                className='py-1 px-2 text-xs font-light leading-4 rounded-2xl border transition-all duration-300 hover:bg-white hover:text-miniBlog-text cursor-pointer'>
+                <span className='font-medium'>#</span>
+                {tag}
+              </p>
+            ))}
+          </div>
+        </div>
       </div>
     </div>
   )
