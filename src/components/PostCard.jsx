7c2f620ec457d90c4f2745ad2a85ce2490154f4d
Felipe F <feferreira.sf@gmail.com>
2022-12-01 23:05:35 -0300

    feat: added tags to the PostCard

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -7,9 +7,10 @@ export const PostCard = ({ postImgUrl, id, postTitle, tags }) => {
         <img src={postImgUrl} alt="" className='w-full h-full' />
       </div>
       <h1 className="text-lg font-medium">{postTitle}</h1>
-      <div className='flex gap-3 self-center'>{tags.map((tag, index) => (
-        <p key={index} className='py-1 px-2 text-sm font-light leading-4 rounded-2xl border transition-all duration-300 hover:bg-white hover:text-miniBlog-text cursor-pointer'><span className="font-medium">#</span>{tag}</p>
-      ))}
+      <div className='flex gap-3 self-center'>
+        {tags.map((tag, index) => (
+          <p key={index} className='py-1 px-2 text-sm font-light leading-4 rounded-2xl border transition-all duration-300 hover:bg-white hover:text-miniBlog-text cursor-pointer'><span className="font-medium">#</span>{tag}</p>
+        ))}
       </div>
       <Link to={`/posts/${id}`} className='w-24 text-lg leading-[0.8] text-white p-2 border transition-all duration-300 hover:bg-white hover:text-miniBlog-bg2'>Ler</Link>
     </div>
