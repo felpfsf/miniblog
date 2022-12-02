febb4d12bb3ad868e3da519dbcc789004ae9076c
Felipe F <feferreira.sf@gmail.com>
2022-12-02 13:57:15 -0300

    feat: added dashboard with user's posts

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,8 +1,27 @@
+import { NoPostFound } from '../../components/NoPostFound'
+import { PostCard } from '../../components/PostCard'
+
+import { useAuthValue } from '../../context/AuthContext'
+import { useFetchPosts } from '../../hooks/useFetchPosts'
 
 export const Dashboard = () => {
+  const { user } = useAuthValue()
+  const uid = user.uid
+  // const posts = []
+  const { posts, loading } = useFetchPosts('posts', null, uid)
   return (
-    <main className="max-w-[1440px] w-full min-h-[calc(100vh_-_208px)] mx-auto pt-16 px-4">
-      <h1 className="text-2xl font-black">Dashboard</h1>
+    <main className='max-w-[1440px] w-full min-h-[calc(100vh_-_208px)] mx-auto mb-4 pt-16 px-4'>
+      <h1 className='text-2xl font-black'>Dashboard</h1>
+      <h2>Gerencie seus posts</h2>
+      {!loading ? <h1>Carregando...</h1> : null}
+      <div className="flex flex-wrap -m-4">
+        {posts ? posts.map(post =>
+          <PostCard key={post.id} {...post} />
+        ) : null}
+      </div>
+      {posts && posts.length === 0 ? (
+        <NoPostFound buttonTitle={'Crie seu primeiro post'} />
+      ) : null}
     </main>
   )
 }
