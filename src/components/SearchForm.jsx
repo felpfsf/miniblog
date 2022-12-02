8b902b018dd1c7027e4db675e0f7d194848eef21
Felipe F <feferreira.sf@gmail.com>
2022-12-02 13:59:07 -0300

    feat: search form logic to show search results

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -1,14 +1,21 @@
 import { useState } from "react"
+import { Navigate, useNavigate } from "react-router-dom"
+import { useFetchPosts } from "../hooks/useFetchPosts"
 
 import { Button } from "./Button"
 import { Input } from "./Input"
 
 export const SearchForm = () => {
   const [searchTerm, setSearchTerm] = useState('')
+  const { posts, loading } = useFetchPosts('posts')
+  const navigate = useNavigate()
 
   const handleSubmit = e => {
     e.preventDefault()
     console.log(searchTerm)
+    if (searchTerm) {
+      return navigate(`/search?q=${searchTerm}`)
+    }
   }
   return (
     <form
