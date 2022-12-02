bbae15a861f88c9e3e31128e7f7292cad6a9d5eb
Felipe F <feferreira.sf@gmail.com>
2022-12-02 12:09:02 -0300

    feat: changes to field names and classnames

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -3,17 +3,17 @@ import { Link } from 'react-router-dom'
 import { HiOutlineArrowSmRight } from 'react-icons/hi'
 import { BsArrowRightShort } from 'react-icons/bs'
 
-export const PostCard = ({ postImgUrl, id, postTitle, tags }) => {
+export const PostCard = ({ headline, imgUrl, id, tags, title }) => {
   return (
-    <div class='p-4 md:w-1/3'>
-      <div class='h-full border-2 border-miniBlog-bg3 rounded-lg overflow-hidden'>
+    <div className='p-4 md:w-1/3'>
+      <div className='h-full border-2 border-miniBlog-bg3 rounded-lg overflow-hidden'>
         <img
-          class='lg:h-48 md:h-36 w-full object-cover object-center'
-          src={postImgUrl}
-          alt={postTitle}
+          className='lg:h-48 md:h-36 w-full object-cover object-center'
+          src={imgUrl}
+          alt={title}
         />
-        <div class='p-6'>
-          <h2 class='tracking-widest text-xs title-font font-medium text-gray-500 uppercase mb-1'>
+        <div className='p-6'>
+          <h2 className='tracking-widest text-xs title-font font-medium text-gray-500 uppercase mb-1'>
             Categoria
           </h2>
           <div className='flex gap-3 self-center'>
@@ -26,17 +26,16 @@ export const PostCard = ({ postImgUrl, id, postTitle, tags }) => {
               </p>
             ))}
           </div>
-          <h1 class='title-font text-lg font-medium text-white my-3'>
-            {postTitle}
+          <h1 className='title-font text-lg font-medium text-white my-3'>
+            {title}
           </h1>
-          <p class='text-white/60 leading-relaxed mb-3'>
-            Photo booth fam kinfolk cold-pressed sriracha leggings jianbing
-            microdosing tousled waistcoat.
+          <p className='text-white/60 leading-relaxed mb-3'>
+            {headline}
           </p>
-          <div class='flex items-center flex-wrap '>
+          <div className='flex items-center flex-wrap '>
             <Link
               to={`/posts/${id}`}
-              class='text-white p-2 border inline-flex items-center md:mb-2 lg:mb-0 duration-300 hover:bg-white hover:text-miniBlog-bg2'>
+              className='text-white p-2 border inline-flex items-center md:mb-2 lg:mb-0 duration-300 hover:bg-white hover:text-miniBlog-bg2'>
               Leia mais
               <BsArrowRightShort size={24} className=' ml-2' />
             </Link>
