c8c8f9bd71ddc9fdbf673ab919744ce20264ab74
Felipe F <feferreira.sf@gmail.com>
2022-12-02 23:22:20 -0300

    feat: add correct time date to post

--- src/pages/Post/Post.jsx
+++ src/pages/Post/Post.jsx
@@ -6,19 +6,27 @@ export const Post = () => {
   const { id } = useParams()
   const { error, loading, post } = useFetchSinglePost('posts', id)
 
-  if (post === null) {
-    return
+  if (post === null) return
+
+  const fbBaseTime = new Date(post.createdAt.seconds * 1000 + post.createdAt.nanoseconds / 1000000)
+  let locale = 'pt-br'
+  let options = {
+    weekday: ('long'),
+    day: ('numeric'),
+    month: ('long'),
+    year: ('numeric'),
   }
-  const fbBaseTime = post.createdAt = new Date(post.createdAt.seconds * 1000 + post.createdAt.nanoseconds / 1000000)
-  // const dateConverted = fbBaseTime.toDateString()
-  console.log(fbBaseTime.toDateString())
+  const dateConverted = fbBaseTime.toDateString()
+  const dateConvertedLocale = fbBaseTime.toLocaleDateString(locale, options)
+  console.log(dateConvertedLocale.toLocaleUpperCase(locale))
+
 
   // console.log(post);
   return (
     <main className="max-w-[1440px] w-full min-h-[calc(100vh_-_208px)] mx-auto pt-16">
       {loading ? <h1 className="text-lg font-light">Carregando post...</h1> : null}
       {post && (
-        <PostDetailed {...post} date={fbBaseTime.toDateString()} />
+        <PostDetailed {...post} date={dateConvertedLocale} />
       )}
     </main>
   )
